241e16a8f9b38443a5eb822682841d74d106cc76
Tatiana Yuryeva <yuryevat@list.ru>
2023-07-04 11:22:26 +0200

    proection added

--- src/js/app.js
+++ src/js/app.js
@@ -7,55 +7,69 @@ widget.bindToDOM();
 const cardsColumns = document.querySelectorAll(".cards");
 let actualCard;
 
+const getStyles = (el) => {
+  return window.getComputedStyle(el);
+};
+
+const proection = (el) => {
+  const div = document.createElement("div");
+  div.classList.add("proection");
+  div.style.height = getStyles(el).height;
+  return div;
+};
+
 for (let cardColumn of cardsColumns) {
   cardColumn.addEventListener("mousedown", (e) => {
     e.preventDefault();
 
     actualCard = e.target;
+    actualCard.style.width = getStyles(actualCard).width;
+    actualCard.style.height = getStyles(actualCard).height;
+
+    const proectionCard = proection(actualCard);
+
+    if (e.target.classList.contains("card")) {
+      actualCard.classList.add("dragged");
+    } else {
+      return;
+    }
 
     let shiftX = e.clientX - actualCard.getBoundingClientRect().left;
     let shiftY = e.clientY - actualCard.getBoundingClientRect().top;
 
-    actualCard.classList.add("dragged");
+    const onMouseMove = (e) => {
+      let target = e.target;
+      let position =
+        target.getBoundingClientRect().height / 2 > e.offsetY
+          ? "beforebegin"
+          : "afterend";
 
-    const onMouseOver = (e) => {
+      if (target.classList.contains("card")) {
+        target.insertAdjacentElement(position, proectionCard);
+      }
+      if (target.classList.contains("cards")) {
+        target.append(proectionCard);
+      }
+
       actualCard.style.top = e.pageY - shiftY + "px";
       actualCard.style.left = e.pageX - shiftX + "px";
     };
 
-    const onMouseUp = (e) => {
-      const mouseUpCard = e.target;
-
-      if (
-        mouseUpCard.parentElement !== null &&
-        mouseUpCard.parentElement.classList.contains("cards")
-      ) {
-        if (e.offsetY < mouseUpCard.getBoundingClientRect().height / 2) {
-          mouseUpCard.parentElement.insertBefore(actualCard, mouseUpCard);
-        } else {
-          mouseUpCard.parentElement.insertBefore(
-            actualCard,
-            mouseUpCard.nextSibling
-          );
-        }
-      }
-      if (mouseUpCard.classList.contains("cards")) {
-        mouseUpCard.append(actualCard);
-      }
-
+    const onMouseUp = () => {
       actualCard.classList.remove("dragged");
-      actualCard.style.top = 0;
-      actualCard.style.left = 0;
+      actualCard.style.top = "unset";
+      actualCard.style.left = "unset";
+      proectionCard.replaceWith(actualCard);
       actualCard = undefined;
 
       document.documentElement.removeEventListener("mouseup", onMouseUp);
-      document.documentElement.removeEventListener("mouseover", onMouseOver);
+      document.documentElement.removeEventListener("mousemove", onMouseMove);
 
       let container = document.querySelector(".container");
       localStorage.setItem("container", container.innerHTML);
     };
 
     document.documentElement.addEventListener("mouseup", onMouseUp);
-    document.documentElement.addEventListener("mouseover", onMouseOver);
+    document.documentElement.addEventListener("mousemove", onMouseMove);
   });
 }
